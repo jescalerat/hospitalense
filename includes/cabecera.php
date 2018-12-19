1c4012831825bfcaf2b4bfd1631f7d7da928bdcf
jescalerat <jet_431@hotmail.com>
2018-12-19 22:02:26 +0100

    Menu

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -36,6 +36,9 @@
         
         <!-- Mapas -->
         <link type="text/css" rel="stylesheet" href="css/leaflet.css"/>
+        
+        <!-- Menu -->
+        <link type="text/css" rel="stylesheet" href="css/menu.css"/>
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
